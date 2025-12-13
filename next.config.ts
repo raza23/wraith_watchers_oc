8af56267b4626e5993ad10b962df7992f9201e92
raza shareef <rtshareef@gmail.com>
2025-12-13 18:20:10 -0500

    better user experience, can view the markers based on the filter in the table

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Disable turbopack to avoid build issues
-  turbopack: false,
 };
 
 export default nextConfig;
